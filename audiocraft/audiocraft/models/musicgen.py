69b6873d2729d46e8eefff2fb3b2405c5c707c2f
DK <dk@MacBook-Air-DK.local>
2026-03-17 02:57:46 +0300

    fix

--- audiocraft/audiocraft/models/musicgen.py
+++ audiocraft/audiocraft/models/musicgen.py
@@ -51,7 +51,10 @@ class MusicGen(BaseGenModel):
     def __init__(self, name: str, compression_model: CompressionModel, lm: LMModel,
                  max_duration: tp.Optional[float] = None):
         super().__init__(name, compression_model, lm, max_duration)
-        self.set_generation_params(duration=15)  # default duration
+        # self.set_generation_params(duration=15)  # default duration
+        default_duration = min(15.0, float(self.max_duration))
+        default_stride = max(1.0, min(18.0, float(self.max_duration) - 0.5))
+        self.set_generation_params(duration=default_duration, extend_stride=default_stride)
 
     @staticmethod
     def get_pretrained(name: str = 'facebook/musicgen-melody', device=None):
